f8a30a536e410c73054600abf2b9d11558cf3572
Jean-Paul Rustom <jeanpaul.r_1999@live.com>
2020-12-17 18:30:31 +0200

    dg

--- configs/secrets-prod.js
+++ configs/secrets-prod.js
@@ -5,6 +5,11 @@
 //     cookieKey:process.env.COOKIE_KEY
 // }
 
+
+const mongoDbUser='jeanpaulrustom';
+const mongoDbPassword='Jeanpaul1999rLUFFY79153043';
+const mongoDbDatabaseName='oauthpractice';
+
 module.exports={
     googleClientID:"638014093776-ci5e6sugeiol5ra1md51dfsrvu8lo878.apps.googleusercontent.com",
     googleClientSecret:"Ox8Q61dzXnvNsQf3EmZPsxkt",
