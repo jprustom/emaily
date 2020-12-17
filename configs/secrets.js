af8fa8f6fc5d8d7cbc27c6feb1351fc37b2a00a7
Jean-Paul Rustom <jeanpaul.r_1999@live.com>
2020-12-17 18:16:27 +0200

    an error occured

--- configs/secrets.js
+++ configs/secrets.js
@@ -1,4 +1,4 @@
 module.exports=
     process.env.NODE_ENV=='production'
-    ?require('./secrets-prod.js')
-    :require('./secrets-dev.js');
+        ?require('./secrets-prod.js')
+        :require('./secrets-dev.js');
